d8ec3f8e19d4d7f00661289f4f74265b87490507
DanielPetrowski <petrowskidaniel@web.de>
2025-11-28 16:23:58 +0100

    add minimalized legal-notice

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { LandingPage } from './landing-page/landing-page';
+import { AboutMe } from './landing-page/about-me/about-me';
+import { MySkills } from './landing-page/my-skills/my-skills';
+import { Portfolio } from './landing-page/portfolio/portfolio';
+import { Contact } from './landing-page/contact/contact';
+import { LegalNotice } from './legal-notice/legal-notice';
 
 export const routes: Routes = [
-    {path:"",component:LandingPage}
+    {path:"",component:LandingPage},
+    {path:"about-me",component:AboutMe},
+    {path:"my-skills",component:MySkills},
+    {path:"portfolio",component:Portfolio},
+    {path:"contact", component: Contact},
+    {path:"legal-notice",component:LegalNotice}
 ];
--- Portfolio/src/app/app.ts
+++ Portfolio/src/app/app.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,inject,} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import {  TranslateService } from '@ngx-translate/core';
+import { LegalNotice } from './legal-notice/legal-notice';
 
-import { LandingPage } from './landing-page/landing-page';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,LandingPage],
+  imports: [RouterOutlet,Header,Footer,LegalNotice],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Portfolio');
+
+     private translate = inject(TranslateService);
+
+  useLanguage(language: string): void {
+      this.translate.use(language);
+  }
 }
