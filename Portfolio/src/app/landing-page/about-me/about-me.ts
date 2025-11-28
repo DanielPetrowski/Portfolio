0d285d8a9ea0a4d53cd1d9ac9b0662ea9cb1b01f
DanielPetrowski <petrowskidaniel@web.de>
2025-11-28 15:55:56 +0100

    add translation & about-me section

---
+++ Portfolio/src/app/landing-page/about-me/about-me.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { TranslatePipe,TranslateService } from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'app-about-me',
+  imports: [TranslatePipe],
+  templateUrl: './about-me.html',
+  styleUrl: './about-me.scss',
+})
+export class AboutMe {
+ private translate = inject(TranslateService);
+
+  useLanguage(language: string): void {
+      this.translate.use(language);
+  }
+}
