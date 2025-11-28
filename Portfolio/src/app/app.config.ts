0a20d2b0ec4bfddfc084f1c5c151261200915746
DanielPetrowski <petrowskidaniel@web.de>
2025-11-28 15:51:19 +0100

    configure Landing-page

--- Portfolio/src/app/app.config.ts
+++ Portfolio/src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection ,inject } from '@angular/core';
+import {provideHttpClient} from "@angular/common/http";
+import {provideTranslateService, TranslateService} from "@ngx-translate/core";
+import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +10,15 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'en',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      })
+    }),
     provideRouter(routes)
   ]
 };
--- Portfolio/src/app/landing-page/landing-page.ts
+++ Portfolio/src/app/landing-page/landing-page.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { AboutMe } from './about-me/about-me';
+import { MySkills } from './my-skills/my-skills';
+import { Portfolio } from './portfolio/portfolio';
+import { Contact } from './contact/contact';
+import { TranslatePipe,TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [AboutMe,MySkills,Portfolio,Contact,TranslatePipe],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
 export class LandingPage {
- 
+  private translate = inject(TranslateService);
+
+  useLanguage(language: string): void {
+      this.translate.use(language);
+  }
 }
